5540d101c0c791e91e1a8ce8c1336c0ceb7fac05
ChettouhMoh <chettouhmoh80@gmail.com>
2025-02-25 10:57:09 +0100

    added simple tab bar to the abb

--- MyFirstExpoApp/app/(tabs)/index.tsx
+++ MyFirstExpoApp/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import { Text, View,  StyleSheet } from 'react-native';
 
 export default function Index() {
@@ -16,6 +17,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   },
 });
