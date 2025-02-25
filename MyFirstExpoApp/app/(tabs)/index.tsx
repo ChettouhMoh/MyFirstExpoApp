4a8ef882d9401d081eded8ae15e2d4c96a431117
ChettouhMoh <chettouhmoh80@gmail.com>
2025-02-25 12:06:27 +0100

    created home page

--- MyFirstExpoApp/app/(tabs)/index.tsx
+++ MyFirstExpoApp/app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import { Link } from 'expo-router';
 import { Text, View,  StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+const PlaceholderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <View style={styles.imageContainer}>
+          <ImageViewer imgSource={PlaceholderImage} />
+          <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme='primary'/>
+        <Button label="Use this photo" />
+      </View>
+      </View>
     </View>
   );
 }
@@ -13,15 +22,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
   },
 });
