5c24eb59bb17dbc7823456f2759128785add3d26
ChettouhMoh <chettouhmoh80@gmail.com>
2025-02-25 08:22:07 +0100

    first commit

---
+++ MyFirstExpoApp/app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+  <View style={styles.container}>
+    <Text style={styles.text}>Hello, Expo! 🚀</Text>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
